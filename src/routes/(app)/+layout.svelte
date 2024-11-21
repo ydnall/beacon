b5c8f57aa224c5a1a4809dbe8eea9561bc3394ed
Andy Li <andyli@kth.se>
2024-11-21 23:59:45 +0100

    chore(routing): adjust base route configuration for website
    - Create a root layout for the website
    - Create a seperate layout for app routes

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Navbar from '$lib/components/Navbar.svelte';
+    import Footer from '$lib/components/Footer.svelte';
+	let { children } = $props();
+</script>
+
+<div>
+    <Navbar />
+	<main>
+		{@render children()}
+	</main>
+    <Footer />
+</div>
