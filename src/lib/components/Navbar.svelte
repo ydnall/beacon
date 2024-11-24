67666ac1f61a04fadaa5c6bf05cc420aea344a60
Andy Li <andyli@kth.se>
2024-11-24 13:22:50 +0100

    feat(ui): implement navbar dropdown menu
    - Add dropdown menu to navbar
    
    Related files:
    - Update Navbar.svelte

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import { Button } from "./ui/button/index";
+	import { Button } from './ui/button/index';
+	import { X } from 'lucide-svelte';
 
 	interface NavItem {
 		href: string;
@@ -14,23 +15,36 @@
 	];
 
 	let isOpen = $state(false);
+
+	function toggleMenu() {
+		isOpen = !isOpen;
+	}
 </script>
 
 {#snippet navItem(href: string, label: string)}
-	<a {href} class="text-muted-foreground hover:text-black transition-colors">{label}</a>
+	<a {href} class="text-muted-foreground transition-colors hover:text-black">{label}</a>
 {/snippet}
 
-<header class="container flex justify-between my-6">
+<header class="container my-6 flex justify-between">
 	<nav>
-		<div class="flex items-center space-x-5">
-			<a href="/" class="flex items-center mr-3">
-				<img src="/beacon.svg" alt="Beacon" class="size-8 mr-2" />
-				<div class="text-xl font-semibold">
-					<span class="hidden md:inline">Beacon</span>
-					<span class="md:hidden">Menu</span>
+		<div class="flex items-center">
+			<a href="/" class="mr-10 hidden items-center md:flex">
+				<img src="/beacon.svg" alt="Beacon" class="mr-2 size-8" />
+				<div class="text-lg font-semibold">
+					<span>Beacon</span>
 				</div>
 			</a>
-			<div class="hidden md:flex items-center space-x-5">
+			<a href="/" class="flex items-center md:hidden" onclick={toggleMenu}>
+				{#if isOpen}
+					<X class="mr-2 size-8" />
+				{:else}
+					<img src="/beacon.svg" alt="Beacon" class="mr-2 size-8" />
+				{/if}
+				<div class="text-lg font-semibold">
+					<span>Menu</span>
+				</div>
+			</a>
+			<div class="hidden items-center space-x-5 md:flex">
 				{#each navItems as { href, label }}
 					{@render navItem(href, label)}
 				{/each}
@@ -41,3 +55,19 @@
 		<Button href="/login" variant="secondary">Login</Button>
 	</nav>
 </header>
+
+{#if isOpen}
+	<div class="absolute top-[5rem] left-4 right-4 z-50 rounded-lg bg-white p-6 shadow-lg md:hidden">
+		<div class="flex flex-col space-y-6">
+			<div class="flex items-center">
+				<img src="/beacon.svg" alt="Beacon" class="mr-2 size-8" />
+				<span class="text-lg font-semibold">Beacon</span>
+			</div>
+			<div class="flex flex-col space-y-4 ml-4">
+				{#each navItems as { href, label }}
+					{@render navItem(href, label)}
+				{/each}
+			</div>
+		</div>
+	</div>
+{/if}
